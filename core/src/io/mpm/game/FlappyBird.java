b59d421207d61491703572d5483db9b98ff55222
kidshenlong <michaelpm91@googlemail.com>
2016-03-11 02:13:52 +0000

    Setup GameStateManager. Using gms in render scope.

--- core/src/io/mpm/game/FlappyBird.java
+++ core/src/io/mpm/game/FlappyBird.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.mpm.game.States.GameStateManager;
+import io.mpm.game.States.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 
@@ -13,21 +15,28 @@ public class FlappyBird extends ApplicationAdapter {
 
 	public static final String TITLE = "Flappy Bird";
 
-	SpriteBatch batch;
-	Texture img;
+	private GameStateManager gsm;
+
+	private SpriteBatch batch;
+	//Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		//img = new Texture("badlogic.jpg");
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+		/*batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
+		batch.end();*/
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 }
---
+++ core/src/io/mpm/game/States/MenuState.java
@@ -0,0 +1,28 @@
+package io.mpm.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Michael on 11/03/2016.
+ */
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
