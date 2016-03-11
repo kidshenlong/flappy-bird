ce1d179af4c0fe66930e94e60ef73ae6adac78f6
kidshenlong <michaelpm91@googlemail.com>
2016-03-11 00:21:19 +0000

    New config constants in FlappyBird class. Desktop launcher now inherits config values from FlappyBird class. Abstract state class to handle different game states,

--- desktop/src/io/mpm/game/desktop/DesktopLauncher.java
+++ desktop/src/io/mpm/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import io.mpm.game.FlappyBird;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyBird.WIDTH;
+		config.height = FlappyBird.HEIGHT;
+		config.title = FlappyBird.TITLE;
 		new LwjglApplication(new FlappyBird(), config);
 	}
 }
