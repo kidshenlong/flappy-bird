bade536ae0cfb8b157269723d535844412f5084a
kidshenlong <michaelpm91@googlemail.com>
2016-03-12 17:03:56 +0000

    Setup background and play button textures.

--- core/src/io/mpm/game/States/MenuState.java
+++ core/src/io/mpm/game/States/MenuState.java
@@ -1,14 +1,21 @@
 package io.mpm.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.mpm.game.FlappyBird;
 
 /**
  * Created by Michael on 11/03/2016.
  */
 public class MenuState extends State {
 
+    private Texture background;
+    private Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
     }
 
     @Override
@@ -23,6 +30,9 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyBird.HEIGHT / 2);
+        sb.end();
     }
 }
