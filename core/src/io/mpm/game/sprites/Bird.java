5b538d76c083dd3e9883ad67c1965896c4ffc4ab
kidshenlong <michaelpm91@googlemail.com>
2016-03-12 17:51:07 +0000

    Added jumping! Stopped bird falling off bottom of screen.

--- core/src/io/mpm/game/sprites/Bird.java
+++ core/src/io/mpm/game/sprites/Bird.java
@@ -22,9 +22,11 @@ public class Bird {
     }
 
     public void update(float dt){
-        velocity.add(0, GRAVITY, 0);
+        if (position.y > 0) velocity.add(0, GRAVITY, 0);
         velocity.scl(dt);
         position.add(0, velocity.y, 0);
+
+        if(position.y < 0) position.y = 0;
 
         velocity.scl(1/dt);
     }
@@ -35,5 +37,9 @@ public class Bird {
 
     public Texture getTexture() {
         return bird;
+    }
+
+    public void jump(){
+        velocity.y = 250;
     }
 }
