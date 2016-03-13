1fd8bb9209d6b84b8bf80f3ebd605088c6924351
kidshenlong <michaelpm91@googlemail.com>
2016-03-13 13:42:43 +0000

    Handling bird stuff in class now instead of playstate. Also drawing tube.

--- core/src/io/mpm/game/States/PlayState.java
+++ core/src/io/mpm/game/States/PlayState.java
@@ -1,29 +1,39 @@
 package io.mpm.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Gdx2DPixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import io.mpm.game.FlappyBird;
+import io.mpm.game.sprites.Bird;
+import io.mpm.game.sprites.Tube;
 
 /**
  * Created by Michael on 12/03/2016.
  */
 public class PlayState extends State{
 
-    private Texture bird;
+    private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);
+        bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()) bird.jump();
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
+        bird.update(dt);
 
     }
 
@@ -31,7 +41,10 @@ public class PlayState extends State{
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
 
     }
